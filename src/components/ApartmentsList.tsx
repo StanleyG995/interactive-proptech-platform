b7226e75ac9db7ba20b9e9757b1f0fd887115a13
Stanley995 <goral.stanislaw95@gmail.com>
2026-08-24 20:00:09 +0200

    feat: add style change for apartment status and apartments mapping

--- src/components/ApartmentsList.tsx
+++ src/components/ApartmentsList.tsx
@@ -1,8 +1,9 @@
 import ApartmentsListItem from "./ApartmentsListItem";
+import { APARTMENTS } from "@/types/apartmentTypes";
 
 const ApartmentsList = () => {
     return (
-        <table className="w-full border-collapse border border-gray-200 text-zinc-800 text-center ">
+        <table className="w-full max-w-[1200px] m-auto border-collapse border border-gray-200 text-zinc-800 text-center ">
             <thead>
                 <tr className="bg-gray-200 text-zinc-800 font-bold">
                     <th className="border border-gray-300 p-2">ID</th>
@@ -15,9 +16,9 @@ const ApartmentsList = () => {
                 </tr>
             </thead>
             <tbody>
-                <ApartmentsListItem id={1} area={75} rooms={3} floor={1} price={150000} pricePerSquareMeter={2000} status="available" />
-                <ApartmentsListItem id={2} area={85} rooms={4} floor={2} price={170000} pricePerSquareMeter={2000} status="sold" />
-                <ApartmentsListItem id={3} area={65} rooms={2} floor={3} price={130000} pricePerSquareMeter={2000} status="reserved" />
+                {APARTMENTS.map((apartment) => (
+                    <ApartmentsListItem key={apartment.id} {...apartment} />
+                ))}
             </tbody>
         </table>
     )
--- src/components/ApartmentsListItem.tsx
+++ src/components/ApartmentsListItem.tsx
@@ -2,7 +2,7 @@ import { ApartmentData } from "@/types/apartmentTypes";
 
 const ApartmentsListItem = ({ id, area, rooms, floor, price, pricePerSquareMeter, status }: ApartmentData) => {
   return (
-    <tr>
+    <tr className={`border border-gray-300 ${status === "sold" ? "bg-red-100" : status === "reserved" ? "bg-yellow-100" : "bg-green-100"}`}>
       <td>{id}</td>
       <td>{area}</td>
       <td>{rooms}</td>
