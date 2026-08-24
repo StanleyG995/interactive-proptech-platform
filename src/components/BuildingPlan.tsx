7ff60e953f7ed8b3c631423a9bc304042eec50bc
Stanley995 <goral.stanislaw95@gmail.com>
2026-08-24 19:04:20 +0200

    feat: add highlight on mobile

--- src/components/BuildingPlan.tsx
+++ src/components/BuildingPlan.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 'use client';
 import { useState } from "react";
 import { FLOORS } from "@/types/buildingTypes";
@@ -6,40 +7,52 @@ const BuildingPlan = () => {
   const [activeFloor, setActiveFloor] = useState<number | null>(null);
 
   return (
-    <div className="building-plan relative w-full">
-      <img
-        src="/building.webp"
-        alt="Building Plan"
-        className="w-[80%] m-auto h-auto block"
-      />
+    <div className="building-plan relative w-full  mx-auto">
+      <div className="relative w-full mx-auto">
+        <img
+          src="/building.webp"
+          alt="Building Plan"
+          className="w-full h-auto block"
+        />
 
-      <svg
-        width="100%"
-        height="100%"
-        viewBox="0 0 1920 1080"
-        version="1.1"
-        xmlns="http://www.w3.org/2000/svg"
-        className="absolute inset-0 w-full h-full pointer-events-none"
-        style={{
-          fillRule: "evenodd",
-          clipRule: "evenodd",
-          strokeLinejoin: "round",
-          strokeMiterlimit: 2,
-        }}
-      >
-        {FLOORS.map((floor) => (
-          <path
-            key={floor.id}
-            d={floor.pathData}
-            className="pointer-events-auto cursor-pointer opacity-0 hover:opacity-50 transition-opacity duration-300"
-            style={{ fill: "#f0690b" }}
-            onClick={() => {
-              setActiveFloor(floor.number);
-              console.log(`Wybrano piętro: ${floor.number}`);
-            }}
-          />
-        ))}
-      </svg>
+        <svg
+          width="100%"
+          height="100%"
+          viewBox="0 0 1920 1080"
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute inset-0 w-full h-full"
+          style={{
+            fillRule: "evenodd",
+            clipRule: "evenodd",
+            strokeLinejoin: "round",
+            strokeMiterlimit: 2,
+          }}
+        >
+          {FLOORS.map((floor) => {
+            const isActive = activeFloor === floor.number;
+
+            return (
+              <path
+                key={floor.id}
+                d={floor.pathData}
+                className="pointer-events-auto cursor-pointer transition-opacity duration-300"
+                style={{
+                  fill: "#f0690b",
+                  opacity: isActive ? 0.6 : 0, 
+                }}
+                onMouseEnter={() => {
+                  setActiveFloor(floor.number);
+                }}
+                onClick={() => {
+                  setActiveFloor(floor.number);
+                  console.log(`${floor.number}`);
+                }}
+              />
+            );
+          })}
+        </svg>
+      </div>
     </div>
   );
 };
