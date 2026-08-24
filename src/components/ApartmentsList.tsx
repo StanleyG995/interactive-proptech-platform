ae66b7b893b587703bf66167e6bf2f9d61cf112a
Stanley995 <goral.stanislaw95@gmail.com>
2026-08-24 22:06:12 +0200

    style: apartments list item

--- src/components/ApartmentsList.tsx
+++ src/components/ApartmentsList.tsx
@@ -3,25 +3,27 @@ import { APARTMENTS } from "@/types/apartmentTypes";
 
 const ApartmentsList = () => {
     return (
-        <table className="w-full max-w-[1300px] m-auto border-collapse border border-gray-200 text-zinc-800 text-center ">
-            <thead>
-                <tr className="bg-gray-200 text-zinc-800 font-bold">
-                    <th className="border border-gray-300 p-2">ID</th>
-                    <th className="border border-gray-300 p-2">Area (m²)</th>
-                    <th className="border border-gray-300 p-2">Rooms</th>
-                    <th className="border border-gray-300 p-2">Floor</th>
-                    <th className="border border-gray-300 p-2">Balcony Area (m²)</th>
-                    <th className="border border-gray-300 p-2">Price ($)</th>
-                    <th className="border border-gray-300 p-2">Price per m² ($)</th>
-                    <th className="border border-gray-300 p-2">Status</th>
-                </tr>
-            </thead>
-            <tbody>
-                {APARTMENTS.map((apartment) => (
-                    <ApartmentsListItem key={apartment.id} {...apartment} />
-                ))}
-            </tbody>
-        </table>
+        <div className="w-full max-w-[1300px] m-auto h-[400px] overflow-y-auto">
+            <table className="w-full border-collapse border border-gray-200 text-zinc-800 text-center">
+                <thead className="sticky top-0 z-10">
+                    <tr className="bg-gray-200 text-zinc-800 font-bold">
+                        <th className="border border-gray-300 p-2 cursor-pointer">ID</th>
+                        <th className="border border-gray-300 p-2 cursor-pointer">Area (m²)</th>
+                        <th className="border border-gray-300 p-2 cursor-pointer">Rooms</th>
+                        <th className="border border-gray-300 p-2 cursor-pointer">Floor</th>
+                        <th className="border border-gray-300 p-2 cursor-pointer">Balcony Area (m²)</th>
+                        <th className="border border-gray-300 p-2 cursor-pointer">Price ($)</th>
+                        <th className="border border-gray-300 p-2 cursor-pointer">Price per m² ($)</th>
+                        <th className="border border-gray-300 p-2 cursor-pointer">Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {APARTMENTS.map((apartment) => (
+                        <ApartmentsListItem key={apartment.id} {...apartment} />
+                    ))}
+                </tbody>
+            </table>
+        </div>
     )
 }
 
