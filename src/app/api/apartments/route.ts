19f0502dff95e70a3738bb48d1237ca9d9c1c781
Stanley995 <goral.stanislaw95@gmail.com>
2026-09-03 21:15:14 +0200

    chore: add http error handling

--- src/app/api/apartments/route.ts
+++ src/app/api/apartments/route.ts
@@ -5,8 +5,9 @@ export async function GET() {
   try {
     const apartments = await getApartments();
     return NextResponse.json(apartments);
-  } catch (error) {
+  } catch (error: unknown) {
     console.error("Błąd bazy danych:", error);
-    return NextResponse.json({ error: 'Błąd serwera' }, { status: 500 });
+    const errorMessage = error instanceof Error ? error.message : String(error);
+    return NextResponse.json({ error: errorMessage }, { status: 500 });
   }
 }
