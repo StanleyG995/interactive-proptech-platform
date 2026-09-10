91414cf00ee2e2bd08896b1774be1d8f277254f7
Stanley995 <goral.stanislaw95@gmail.com>
2026-09-10 19:17:30 +0200

    style(building-plan): update tooltip appearance

--- src/components/BuildingPlan.tsx
+++ src/components/BuildingPlan.tsx
@@ -96,23 +96,23 @@ const BuildingPlan = () => {
         {/* Tooltip */}
         {activeFloor && stats && (
           <div
-            className="absolute z-50 pointer-events-none bg-white/80 text-zinc-800 px-4 py-3 rounded-lg shadow-sm text-sm backdrop-blur-sm border border-gray-200 transition-all duration-75 ease-out"
+            className="absolute z-50 pointer-events-none bg-white/80 text-zinc-800 rounded-lg shadow-md text-sm backdrop-blur-sm transition-all duration-75 ease-out overflow-hidden"
             style={{
               left: `${mousePos.x + 15}px`,
               top: `${mousePos.y + 15}px`,
               transform: "translate(0, 0)",
             }}
           >
-            <div className="flex justify-between items-center mb-2">
-              <div className="font-semibold text-2xl text-[#f0690b] mb-1  pb-1">
+            <div className="flex justify-between items-center bg-black/7 rounded-t-lg">
+              <div className="font-semibold text-2xl bg-[#f0690b] px-3 py-2 text-white ">
                 {activeFloor.building}
               </div>
-              <div className="font-semibold text-2xl mb-1 pb-1">
-                P-{activeFloor.number}
+              <div className="font-hairline text-lg text-zinc-500/70 uppercase tracking-wider pr-3 py-2">
+                P{activeFloor.number}
               </div>
             </div>
 
-            <div className="space-y-1 flex flex-col gap-2">
+            <div className="space-y-1 flex flex-col gap-2 px-3 py-2">
               <div className="flex justify-between text-zinc-600 uppercase text-sm tracking-wider gap-6">
                 Wolne{" "}
                 <span className="font-bold text-emerald-700">
